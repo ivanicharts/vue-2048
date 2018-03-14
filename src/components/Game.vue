6cabd12a5b3ba6fe088780dc9e796956ee3f756a
Ivan Botnari <ivan.botnari@titanium-soft.com>
2018-03-14 09:42:25 +0200

    conflict

---
+++ src/components/Game.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="content">
+    <h1 class="title">kerob</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Game2048',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.title {
+  color: #5df3af;
+}
+</style>
